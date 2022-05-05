8e9a9f4050c5073cfc3a98a73925fb3a2315ebc8
Raul Luis <soyrauluis@gmail.com>
2022-05-04 23:08:32 -0400

    Update MainActivity

--- app/src/main/java/com/devaruluis/mvvm/ui/view/MainActivity.kt
+++ app/src/main/java/com/devaruluis/mvvm/ui/view/MainActivity.kt
@@ -1,14 +1,15 @@
 package com.devaruluis.mvvm.ui.view
 
+import android.graphics.Color
 import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.isVisible
 import androidx.lifecycle.Observer
 import com.devaruluis.mvvm.databinding.ActivityMainBinding
+import com.devaruluis.mvvm.ui.viewmodel.ColorViewModel
 import com.devaruluis.mvvm.ui.viewmodel.QuoteViewModel
 import dagger.hilt.android.AndroidEntryPoint
-import dagger.hilt.android.HiltAndroidApp
 
 
 @AndroidEntryPoint
@@ -16,6 +17,8 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
 
     private val quoteViewModel: QuoteViewModel by viewModels()
+    private val colorViewModel: ColorViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -23,14 +26,30 @@ class MainActivity : AppCompatActivity() {
         setContentView(view)
 
         quoteViewModel.onCreate()
+        colorViewModel.onCreate()
 
         quoteViewModel.quoteModel.observe(this, Observer {
             binding.tvQuote.text = it?.quote
             binding.tvAuthor.text = it?.author
         })
 
-        quoteViewModel.isLoading.observe(this, Observer { binding.loading.isVisible = it })
+        colorViewModel.colorModel.observe(this, Observer {
+            val color: Int = try {
+                Color.parseColor("#" + it?.hex)
 
-        binding.viewContainer.setOnClickListener { quoteViewModel.randomQuote() }
+            } catch (e: NumberFormatException) {
+                Color.parseColor("#000000")
+            }
+            binding.tvQuote.setTextColor(color)
+            binding.tvAuthor.setTextColor(color)
+        })
+
+        quoteViewModel.isLoading.observe(this, Observer { binding.loading.isVisible = it })
+        colorViewModel.isLoading.observe(this, Observer { binding.loading.isVisible = it })
+
+        binding.viewContainer.setOnClickListener {
+            quoteViewModel.randomQuote()
+            colorViewModel.randomColor()
+        }
     }
 }
