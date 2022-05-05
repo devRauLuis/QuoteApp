e0792a19850646be3d253b37ded8dc957ef57be7
Raul Luis <soyrauluis@gmail.com>
2022-05-04 23:08:11 -0400

    Update to support Colr API

--- app/src/main/java/com/devaruluis/mvvm/di/NetworkModule.kt
+++ app/src/main/java/com/devaruluis/mvvm/di/NetworkModule.kt
@@ -1,5 +1,6 @@
 package com.devaruluis.mvvm.di
 
+import com.devaruluis.mvvm.data.network.ColrApiClient
 import com.devaruluis.mvvm.data.network.QuoteApiClient
 import dagger.Module
 import dagger.Provides
@@ -9,26 +10,61 @@ import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Named
 import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 object NetworkModule {
+    private const val ZEN_QUOTES_API_URL = "https://zenquotes.io/"
+    private const val COLR_API_URL = "https://www.colr.org/"
+
+    @Provides
+    @Named("zenQuotesGson")
+    fun provideZenQuotesGson(): GsonConverterFactory = GsonConverterFactory.create()
+
+    @Provides
+    @Named("colrGson")
+    fun provideColrGson(): GsonConverterFactory = GsonConverterFactory.create()
+
     @Singleton
     @Provides
-    fun provideRetrofit(): Retrofit {
+    fun provideOkHttpClient(): OkHttpClient {
         val logging = HttpLoggingInterceptor()
         logging.setLevel(HttpLoggingInterceptor.Level.BODY)
-        val client = OkHttpClient().newBuilder()
-            .addInterceptor(logging)
-
-        return Retrofit.Builder().baseUrl("https://zenquotes.io/")
-            .addConverterFactory(GsonConverterFactory.create()).client(client.build()).build()
+        return OkHttpClient().newBuilder()
+            .addInterceptor(logging).build()
     }
 
     @Singleton
     @Provides
-    fun provideQuoteApiClient(retrofit: Retrofit): QuoteApiClient {
+    @Named("zenQuotesApiRetrofit")
+    fun provideZenQuotesApiRetrofit(
+        @Named("zenQuotesGson") gson: GsonConverterFactory,
+        okHttpClient: OkHttpClient
+    ): Retrofit =
+        Retrofit.Builder().baseUrl(ZEN_QUOTES_API_URL)
+            .addConverterFactory(gson).client(okHttpClient).build()
+
+    @Singleton
+    @Provides
+    @Named("colrApiRetrofit")
+    fun provideColrApiRetrofit(
+        @Named("colrGson") gson: GsonConverterFactory,
+        okHttpClient: OkHttpClient
+    ): Retrofit = Retrofit.Builder().baseUrl(
+        COLR_API_URL
+    ).addConverterFactory(gson).client(okHttpClient).build()
+
+    @Provides
+    @Singleton
+    fun provideQuoteApiClient(@Named("zenQuotesApiRetrofit") retrofit: Retrofit): QuoteApiClient {
         return retrofit.create(QuoteApiClient::class.java)
+    }
+
+    @Provides
+    @Singleton
+    fun provideColrApiClient(@Named("colrApiRetrofit") retrofit: Retrofit): ColrApiClient {
+        return retrofit.create(ColrApiClient::class.java)
     }
 }
